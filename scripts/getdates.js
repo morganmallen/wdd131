c6d45f13a3f464b6096b2bf8e5f13258ed7a51d4
Morgan Allen <morganallen1999@gmail.com>
2024-06-14 17:07:48 -0600

    home page css, html, and javascript

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModification = new Date(document.lastModified);
+
+document.getElementById("lastModified").textContent = lastModification;
