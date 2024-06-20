7a65047628b9c01a65ba427074bbd0c53570c9b1
Morgan Allen <morganallen1999@gmail.com>
2024-06-19 22:55:57 -0600

    almost done

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModification = new Date(document.lastModified);
+document.getElementById("lastModified").textContent = lastModification;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
